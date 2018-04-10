65b14acdd5006a16f4551128bcbc033bfe4627f1
duoyi-window <1323242382@qq.com>
2018-04-10 21:47:40 +0800

    35zww

--- crawler/zww35.py
+++ crawler/zww35.py
@@ -7,11 +7,10 @@
     http://www.35zww.com/zwwtopallvisit/0/0.htm
 """
 
-import pubcrawler
 import re
 import os
 
-from pubcode import misc
+from pubcode import misc, pubcrawler
 from bs4 import BeautifulSoup
 
 DEL_LINE = [
@@ -25,20 +24,40 @@ DEL_LINE = [
     "www.35zｗｗ.com，更新快、无弹窗！        ",
 ]
 
+ERROR_TITLE = "404 - 页面不存在 - 三五中文网"
 
+WAITING_URL_KEYNAME = "WaitSubUrl"  #等待顺序写入book的url
+DONE_URL_KEYNAME = "DoneSubUrl"     #已经写入book的url
 
 class CZww35(pubcrawler.CPubCrawler):
-    m_Flag = "zww35"
+    m_Flag = "book"
+    m_DebugPrint = True
     m_Example = "http://www.35zww.com/zwwtopallvisit/0/"
     m_Url = "http://www.35zww.com/"
-
+    m_TryNum = 5    #尝试url的次数
 
     def _CustomInit(self):
-        # for x in range(2146):
+        self._LoadWaitUrl()
         for x in range(10):
             pageurl = self.m_Example + str(x) + ".htm"
-            dPageInfo = {"priority":0}
+            dPageInfo = {
+                "priority"  :0,
+                "time"      :misc.GetSecond(),
+            }
             self.m_WaitingUrl[pageurl] = dPageInfo
+
+
+    def _LoadWaitUrl(self):
+        for bookurl, dBookInfo in self.m_DoneInfo.items():
+            for chapterurl in dBookInfo.get(WAITING_URL_KEYNAME, []):
+                if chapterurl in self.m_DoneInfo:
+                    continue
+                dChapterInfo = {
+                    "priority"  :2,
+                    "parent"    :bookurl,
+                    "time"      :misc.GetSecond(),
+                }
+                self.m_WaitingUrl[chapterurl] = dChapterInfo
 
 
     def _Replace(self, sMsg, default="_"):
@@ -47,19 +66,45 @@ class CZww35(pubcrawler.CPubCrawler):
         return sMsg
 
 
+
+    def Is404(self, url, dInfo, soup):
+        sTitle = soup.find("title").text
+        if sTitle != ERROR_TITLE:    #有内容
+            return False
+        iNum = dInfo.get("num", 0) + 1
+        dInfo["num"] = iNum
+        dInfo["time"] = dInfo.get("time", misc.GetSecond() - 3) + 3
+        if iNum < self.m_TryNum:
+            self.m_WaitingUrl[url] = dInfo
+        else:
+            iType = dInfo["priority"]
+            if iType == 3:
+                bookurl = dInfo["parent"]
+                lstAllUrl = self.m_DoneInfo[bookurl].get(WAITING_URL_KEYNAME, [])
+                if url in lstAllUrl:
+                    lstAllUrl.remove(url)
+                self.CheckWriteBook(bookurl)
+            misc.Write2File(self.m_LogPath, "abandon %s %s" % (url, dInfo))
+
+        del self.m_DoingUrl[url]
+        return True
+
+
     async def Parse(self, url, dInfo, html):
         iType = dInfo["priority"]
-        if iType == 0:
-            await self.ParsePage(url, dInfo, html)
-        elif iType == 1:
-            await self.ParseBook(url, dInfo, html)
-        elif iType == 2:
-            await self.ParseChapter(url, dInfo, html)
-
-
-    async def ParsePage(self, pageurl, dPageInfo, html):
         soup = BeautifulSoup(html, 'lxml')
-        allBookUrl = dPageInfo.setdefault("allurl", [])
+        if self.Is404(url, dInfo, soup):
+            return
+        if iType == 0:
+            await self.ParsePage(url, dInfo, soup)
+        elif iType == 1:
+            await self.ParseBook(url, dInfo, soup)
+        elif iType == 2:
+            await self.ParseChapter(url, dInfo, soup)
+
+
+    async def ParsePage(self, pageurl, dPageInfo, soup):
+        allBookUrl = dPageInfo.setdefault(WAITING_URL_KEYNAME, [])
         for oA in soup.findAll("a", {"href":re.compile("/35zwhtml/\d+/\d+/$")}):
             if oA.has_attr("style"):
                 continue
@@ -74,6 +119,7 @@ class CZww35(pubcrawler.CPubCrawler):
             dBookInfo = {
                 "priority"  :1,
                 "parent"    :pageurl,
+                "time"      :misc.GetSecond(),
             }
             self.m_WaitingUrl[bookurl] = dBookInfo
             # print("Add Book url ", bookurl)
@@ -84,9 +130,9 @@ class CZww35(pubcrawler.CPubCrawler):
         print("本页【%s】爬取完毕, 共需爬取%s本书籍" % (pageurl, len(allBookUrl)))
 
 
-    async def ParseBook(self, bookurl, dBookInfo, html):
-        soup = BeautifulSoup(html, 'lxml')
-        sTitle = soup.find("meta", {"property":"og:title"}).get("content")
+    async def ParseBook(self, bookurl, dBookInfo, soup):
+        oTitle = soup.find("meta", {"property":"og:title"})
+        sTitle = oTitle.get("content")
         sTitle = self._Replace(sTitle)
         dBookInfo["title"] = sTitle
         oDetails = soup.find("div", {"id":"details"})
@@ -100,12 +146,11 @@ class CZww35(pubcrawler.CPubCrawler):
         sType = oType.find("a", {"href":re.compile(".*htm")}).text
         dBookInfo["type"] = sType
 
-        allChapterUrl = dBookInfo.setdefault("allurl", [])
+        allChapterUrl = dBookInfo.setdefault(WAITING_URL_KEYNAME, [])
         for oA in soup.findAll("a", {"href":re.compile("\d+.html")}):
             if not oA.has_attr("title"):
                 continue
             chapterurl = bookurl + oA.get("href")
-
             if chapterurl in allChapterUrl:
                 continue
 
@@ -119,21 +164,12 @@ class CZww35(pubcrawler.CPubCrawler):
             self.m_WaitingUrl[chapterurl] = dChapterInfo
 
         self.m_DoneInfo[bookurl] = self.m_DoingUrl.pop(bookurl)
-        print("本书【%s/%s】爬取完毕, 共需爬取%s章节" % (sType, sTitle, len(allChapterUrl)))
+        print("%s-《%s》共需爬取%s章节" % (sType, sTitle, len(allChapterUrl)))
 
 
-    async def ParseChapter(self, chapterurl, dChapterInfo, html):
+    async def ParseChapter(self, chapterurl, dChapterInfo, soup):
         bookurl = dChapterInfo["parent"]
-        soup = BeautifulSoup(html, 'lxml')
         oDiv = soup.find("div", {"id":"title"})
-        if not oDiv:
-            misc.Write2File(self.m_LogPath, "%s %s" % (chapterurl, dChapterInfo))
-            self.m_DoingUrl.pop(chapterurl)
-            lstAllUrl = self.m_DoneInfo[bookurl].get("allurl", [])
-            if chapterurl in lstAllUrl:
-                lstAllUrl.remove(chapterurl)
-            self.CheckWriteBook(bookurl)
-            return
         sChapterTitle = oDiv.h1.text
 
         oScript = soup.find("div", id="content")
@@ -146,14 +182,15 @@ class CZww35(pubcrawler.CPubCrawler):
         dChapterInfo["chapter_title"] = sChapterTitle
 
         self.m_DoneInfo[chapterurl] = self.m_DoingUrl.pop(chapterurl)
-        print("\t本章节[%s]爬取完毕" % (sChapterTitle))
+        print("\t本章节<--%s-->爬取完毕" % (sChapterTitle))
 
         self.CheckWriteBook(bookurl)
 
 
     def CheckWriteBook(self, bookurl):
         dBookInfo = self.m_DoneInfo[bookurl]
-        lstAllUrl = dBookInfo["allurl"]
+        lstAllUrl = dBookInfo[WAITING_URL_KEYNAME]
+        lstDoneUrl = dBookInfo.setdefault(DONE_URL_KEYNAME, [])
         sTitle = dBookInfo["title"]
         sType = dBookInfo["type"]
         sDir = os.path.join(self.m_DownPath, sType)
@@ -163,6 +200,10 @@ class CZww35(pubcrawler.CPubCrawler):
         with open(sFilePath, "a+", encoding="utf-8") as fp:
             while lstAllUrl and lstAllUrl[0] in self.m_DoneInfo:
                 chapterurl = lstAllUrl.pop(0)
+                if chapterurl in lstDoneUrl:
+                    misc.Write2File(self.m_LogPath, "Err: %s已经在书籍中了" % (chapterurl))
+                    continue
+                lstDoneUrl.append(chapterurl)
                 dChapterInfo = self.m_DoneInfo.pop(chapterurl)
                 sChapterTitle = dChapterInfo["chapter_title"]
                 sText = dChapterInfo["text"]
@@ -170,7 +211,7 @@ class CZww35(pubcrawler.CPubCrawler):
                 fp.writelines("=============%s=============\n" % sChapterTitle)
                 fp.writelines(sText)
                 fp.writelines("\n"*3)
-                print("\t【%s】 ————> %s/%s.txt" % (sChapterTitle, sType, sTitle))  #TODO
+                print("\t【%s】 ————> %s/%s.txt" % (sChapterTitle, sType, sTitle))
 
         if not lstAllUrl:
             self.FinishDownlandBook(bookurl)
@@ -185,5 +226,5 @@ class CZww35(pubcrawler.CPubCrawler):
             "title"                 :sTitle
         }
         self.m_DoneInfo[bookurl] = dNewBookInfo
-        print("%s.txt 下载完毕" % sTitle)
+        print("《%s.txt》 下载完毕" % sTitle)
 
